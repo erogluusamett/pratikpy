23b65e2d6e8bf3a8edac0eaedbaf0cb54b84b406
erogluusamett <eroglu_samet@hotmail.com>
2025-04-08 01:53:00 +0300

    ddds

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "deneme1234 aaa bbb"
+    return "deneme1234 aaa bbbdd"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80)
