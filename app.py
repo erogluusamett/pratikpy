30e6979315e54e41038d495b337d0c309a8a8fac
erogluusamett <eroglu_samet@hotmail.com>
2025-04-08 01:57:12 +0300

    sss

--- app.py
+++ app.py
@@ -8,3 +8,10 @@ def home():
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80)
+
+
+
+
+# Uygulamayı Docker ile çalıştırmak için terminalde sırasıyla şu komutları çalıştır:
+# docker build -t flask-deneme .
+# docker run -d -p 5050:80 --name flask-test flask-deneme
